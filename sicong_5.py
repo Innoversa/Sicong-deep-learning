6be323ef4500e9ff75a5b6a00fa5d4c709625ea0
Sicong_mac <siconghuang@tamu.edu>
2020-03-24 18:43:32 -0500

    Create sicong_5.py

---
+++ sicong_5.py
@@ -0,0 +1,12 @@
+import csv
+import numpy as np
+from sklearn import svm
+import sklearn
+
+# start of data processing part
+with open('my_pics/json_datas/ans_all_15_images.csv', mode = 'r') as data_file:
+    data_reader = csv.reader(data_file, delimiter=',', quotechar='\'', quoting=csv.QUOTE_NONNUMERIC)
+    ans_data = []
+    for each in data_reader:
+        ans_data = each
+    ans_data = list(map(int, ans_data))
