3d96538ae42fa97079542d4efd5e7c46772d44f1
Sicong_mac <siconghuang@tamu.edu>
2020-03-24 19:56:40 -0500

    refactoring the pngs

--- sicong_5.py
+++ sicong_5.py
@@ -4,7 +4,7 @@ from sklearn import svm
 import sklearn
 
 # start of data processing part
-with open('my_pics/json_datas/ans_all_15_images.csv', mode = 'r') as data_file:
+with open('my_/json_datas/ans_all_15_images.csv', mode = 'r') as data_file:
     data_reader = csv.reader(data_file, delimiter=',', quotechar='\'', quoting=csv.QUOTE_NONNUMERIC)
     ans_data = []
     for each in data_reader:
